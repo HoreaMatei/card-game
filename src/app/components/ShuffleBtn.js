45c38b2051c3ab92ca30bb9e970e2bea769d2f5b
HoreaMatei <mateihorea17@yahoo.com>
2024-07-03 19:45:36 +0300

    first commit

---
+++ src/app/components/ShuffleBtn.js
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+import { Button } from '@nextui-org/react'
+
+const ShuffleBtn = ({shuff}) => {
+  return (
+    <div>
+        <Button onClick={shuff} >
+            Shufflesssssssssssss
+        </Button>
+    </div>
+  )
+}
+
+export default ShuffleBtn
