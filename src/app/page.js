65aff4cbdc64a07464aea0b6e076d693fbe1b8ce
HoreaMatei <mateihorea17@yahoo.com>
2024-07-03 23:22:00 +0300

    new commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,19 @@
 'use client'
+import './globals.css'
 
 import Image from "next/image";
 import styles from "./page.module.css";
 import useSWR from "swr"
 
 import { Button } from "@nextui-org/react";
+import { useState } from "react";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 
 export default  function Home() {
-  
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
 
 
  //const data2 = await fetch('https://api.storyblok.com/v2/cdn/stories/rings?version=draft&token=OQ09pa2LLqe7rgabggVtmQtt&cv=1720014571')
@@ -25,12 +28,15 @@ export default  function Home() {
 
 
   const img = data.story.content.body[0].columns[0].image
-  const arrayS = [img.map((item)=>item.filename)]
+  const arrayS = img.map((item)=>item.filename)
   
    
   const shuffleCards =  ()=>{
-    const shuffledCards = [...arrayS, ...arrayS].sort(()=> Math.random() + 1)
-    console.log(shuffledCards)  
+    const cardsArray = [...arrayS, ...arrayS]
+    const shuffledCards = cardsArray.sort(()=>0.5 - Math.random())
+    setCards(shuffledCards)
+
+    console.log(cards)  
  
   
    
@@ -42,10 +48,24 @@ export default  function Home() {
     <Button onClick={shuffleCards}>shuffle </Button>   
  
      {data.story.content.body[0].columns[0].title}
+     <div className="card-grid">
+     
+          {cards.map(card=> (
+            <div className="card" key={card.id}>
+              <div>
+                <Image height={250} width={250} className="front" src={card} alt= 'card front' />
+                <Image height={250} width={250} className="back" src="/lotr.avif" alt="card back" />
+              </div>
+            </div>
+          ))}
+       
+       
+
+     </div>
 
      
   
-     {img.map((imgg) =><div> <Image  height={200} width={200} src= {imgg.filename} />  <Image  src="/lotr.avif" height={200} width={200}/></div>)}
+    
     </main>
   );
 }
